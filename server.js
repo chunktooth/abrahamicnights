6e99844e1cd39af49c4cfb97cd3cca8d6380e525
chunktooth <om.saetehn@gmail.com>
2018-08-22 22:30:55 -0700

    why not

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
-var createError = require('http-errors');
+
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-// var indexRouter = require('./routes/index');
-
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('root')
-})
+app.set('port')
+app.use("/", express.static(path.join(__dirname, 'build')));
 
-app.use(express.static(path.join(__dirname, 'build')));
 
 module.exports = app;
