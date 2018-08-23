08ac7512a6feb9626e8b7df0938c59a3aa8c4696
chunktooth <om.saetehn@gmail.com>
2018-08-22 22:49:11 -0700

    why not

--- server.js
+++ server.js
@@ -4,11 +4,10 @@ var app = express();
 
 app.set('port');
 
-app.use(express.compress());
 app.use("/", express.static(path.join(__dirname, 'build')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + './build/index.html'));
-})
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname + './build/index.html'));
+// })
 
 module.exports = app;
