d08211a5d15dd1a734a052d35d4ec12fd203deb2
chunktooth <om.saetehn@gmail.com>
2018-09-12 00:33:28 -0700

    Updating site

--- src/data/vids.js
+++ src/data/vids.js
@@ -133,9 +133,7 @@ const vids = [
   {
     "note": "NOTE- Since the time of writing Ramadan has faced multiple accusations of sexual harrassment so his prestige has taken a hit.",
     "intro": "",
-    "url": [ 
-      ""
-    ]
+    "url": []
   },
 
   {
